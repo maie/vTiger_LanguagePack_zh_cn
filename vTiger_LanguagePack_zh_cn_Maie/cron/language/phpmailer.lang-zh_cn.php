3954efe6942499d7baddac02ead2e3b9a665e8ad
maie <m@maie.name>
2014-03-13 14:14:01 +0800

    6.0.0版
    6.0.0版简体中文语言�

---
+++ vTiger_LanguagePack_zh_cn_Maie/cron/language/phpmailer.lang-zh_cn.php
@@ -0,0 +1,30 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+* ("License"); You may not use this file except in compliance with the License
+* The Original Code is:  vtiger CRM Open Source
+* The Initial Developer of the Original Code is vtiger.
+* Portions created by vtiger are Copyright (C) vtiger.
+* 简体中文语言包 - 定时邮件
+ * 版本: 6.0.0 
+ * 作者: Maie | www.maie.name
+ * 更新日期: 2014-01-13
+* All Rights Reserved.
+********************************************************************************/
+
+$PHPMAILER_LANG = array();
+$PHPMAILER_LANG["provide_address"] = '您必须提供至少一个 ' .
+                                     '收件人的电子邮件地址.';
+$PHPMAILER_LANG["mailer_not_supported"] = '邮件不被支持.';
+$PHPMAILER_LANG["execute"] = '无法执行: ';
+$PHPMAILER_LANG["instantiate"] = '不能实例化的邮件功能.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP错误：无法验证.';
+$PHPMAILER_LANG["from_failed"] = '以下发送地址失败: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP错误：以下 ' .
+                                       '收件人失败: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP错误：数据不被接受.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP错误：无法连接到SMTP服务器.';
+$PHPMAILER_LANG["file_access"] = '无法接受文件: ';
+$PHPMAILER_LANG["file_open"] = '文件错误：无法打开文件: ';
+$PHPMAILER_LANG["encoding"] = '未知编码: ';
+?>
